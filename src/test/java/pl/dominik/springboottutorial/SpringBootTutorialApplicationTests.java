35b39818a609ce068f2388a4a7308ce9237ea6c8
kanthakdominik <kanthakdominik@gmail.com>
2021-11-17 00:16:07 +0100

    added tests written in Rest Assured framework

---
+++ src/test/java/pl/dominik/springboottutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dominik.springboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
